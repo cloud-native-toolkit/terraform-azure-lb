6e815ea81c566506ada79a57376eedd4c87610f5
Saurabh Rajguru <saurabh.rajguru@gmail.com>
2022-06-06 11:11:16 +0530

    Added ability to use existing public ip (#18)
    * Added ability to use existing public ip
    
    Added ability to use existing public ip.
    Formatted test files.
    
    Signed-off-by: Saurabh Rajguru <saurabh.rajguru@ibm.com>
    
    * Added explicit IP Version in respective resource.
    
    Added explicit IP Version in respective resource.
    
    Signed-off-by: Saurabh Rajguru <saurabh.rajguru@ibm.com>

--- test/stages/stage1-subnet.tf
+++ test/stages/stage1-subnet.tf
@@ -2,15 +2,15 @@ module "subnets" {
   source = "github.com/cloud-native-toolkit/terraform-azure-subnets"
 
   resource_group_name = module.resource_group.name
-  region = var.region
-  vpc_name = module.vnet.name
-  _count = 2
-  ipv4_cidr_blocks = ["10.0.1.0/24", "10.0.2.0/24"]
-  enabled = var.enabled
-  acl_rules = []
+  region              = var.region
+  vpc_name            = module.vnet.name
+  _count              = 2
+  ipv4_cidr_blocks    = ["10.0.1.0/24", "10.0.2.0/24"]
+  enabled             = var.enabled
+  acl_rules           = []
 }
 
-resource null_resource print_enabled {
+resource "null_resource" "print_enabled" {
   provisioner "local-exec" {
     command = "echo -n '${module.subnets.enabled}' > .enabled"
   }
