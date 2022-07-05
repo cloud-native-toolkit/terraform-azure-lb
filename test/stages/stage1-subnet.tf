161b3b45a9f2d9e83afcb03e47654c1f1d8ef576
Rich Ehrhardt <51330294+rich-ehrhardt@users.noreply.github.com>
2022-07-05 12:58:29 +1000

    Changed public IP default to standard (#24)
    * Changed public IP default to standard
    
    Signed-off-by: Rich Ehrhardt <rich_ehrhardt@au1.ibm.com>
    
    * Removed enabled flag from test stages
    
    Signed-off-by: Rich Ehrhardt <rich_ehrhardt@au1.ibm.com>

--- test/stages/stage1-subnet.tf
+++ test/stages/stage1-subnet.tf
@@ -6,12 +6,6 @@ module "subnets" {
   vpc_name            = module.vnet.name
   _count              = 2
   ipv4_cidr_blocks    = ["10.0.1.0/24", "10.0.2.0/24"]
-  enabled             = var.enabled
   acl_rules           = []
 }
 
-resource "null_resource" "print_enabled" {
-  provisioner "local-exec" {
-    command = "echo -n '${module.subnets.enabled}' > .enabled"
-  }
-}
