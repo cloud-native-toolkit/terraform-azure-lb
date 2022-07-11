4d66fdda3dca5b1636aba8b79027300dad2c8ba8
Rich Ehrhardt <rich_ehrhardt@au1.ibm.com>
2022-07-11 10:10:19 +1000

    Added backend pool id to outputs
    Signed-off-by: Rich Ehrhardt <rich_ehrhardt@au1.ibm.com>

--- outputs.tf
+++ outputs.tf
@@ -55,7 +55,7 @@ output "backend_address_id_v6" {
 }
 
 output "public_ip_address_v4" {
-    description = "Public IP allocated to the load balancer"
+    description = "Public IP (IPv4) allocated to the load balancer"
     value       = var.public && var.use_ipv4 ? var.public_ip_name_v4 == "" ? azurerm_public_ip.load_balancer_v4[0].ip_address : data.azurerm_public_ip.public_ip_v4[0].ip_address : null
     depends_on  = [
       azurerm_public_ip.load_balancer_v4
@@ -63,7 +63,7 @@ output "public_ip_address_v4" {
 }
 
 output "public_ip_address_v6" {
-    description = "Public IP allocated to the load balancer"
+    description = "Public IP (IPv6) allocated to the load balancer"
     value       = var.public && var.use_ipv6 ? var.public_ip_name_v6 == "" ? azurerm_public_ip.load_balancer_v6[0].ip_address : data.azurerm_public_ip.public_ip_v6[0].ip_address : null
     depends_on  = [
       azurerm_public_ip.load_balancer_v6
@@ -73,7 +73,7 @@ output "public_ip_address_v6" {
 output "public_fqdn_v4" {
   description = "FQDN allocated to IPv4 public IP address"
   value       = var.public && var.use_ipv4 && var.create_fqdn ? var.public_ip_name_v4 == "" ? azurerm_public_ip.load_balancer_v4[0].fqdn : data.azurerm_public_ip.public_ip_v4[0].fqdn : null
-  depends_on = [
+  depends_on  = [
     azurerm_public_ip.load_balancer_v4
   ]
 }
@@ -81,7 +81,23 @@ output "public_fqdn_v4" {
 output "public_fqdn_v6" {
   description = "FQDN allocated to IPv6 public IP address"
   value       = var.public && var.use_ipv6 ? var.public_ip_name_v6 == "" ? azurerm_public_ip.load_balancer_v6[0].fqdn : data.azurerm_public_ip.public_ip_v6[0].fqdn : null
-  depends_on = [
-    azurerm_public_ip.load_balancer_v4
+  depends_on  = [
+    azurerm_public_ip.load_balancer_v6
+  ]
+}
+
+output "backend_pool_id_v4" {
+  description = "Backend pool ID for IPv4"
+  value       = azurerm_lb_backend_address_pool.load_balancer_v4[0].id
+  depends_on  = [
+    azurerm_lb_backend_address_pool.load_balancer_v4
+  ]
+}
+
+output "backend_pool_id_v6" {
+  description = "Backend pool ID for IPv6"
+  value       = azurerm_lb_backend_address_pool.load_balancer_v6[0].id
+  depends_on  = [
+    azurerm_lb_backend_address_pool.load_balancer_v6
   ]
 }
